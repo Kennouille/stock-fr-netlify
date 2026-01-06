6b38240e8e9f5ad5e9d6daad912d58dfd42ea888
Kennouille <muhlemannk@gmail.com>
2026-01-06 11:45:35 -0600

    Amélioration

--- netlify/fonctions/vuestock-api.js
+++ netlify/fonctions/vuestock-api.js
@@ -32,7 +32,7 @@ exports.handler = async (event) => {
       console.log('Saving to Supabase:', body);
 
       const supabaseUrl = 'https://mngggybayjooqkzbhvqy.supabase.co';
-      const supabaseKey = 'TON_SUPABASE_KEY_ICI';
+      const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZ2dneWJheWpvb3FremJodnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2MTU3NDgsImV4cCI6MjA0MjE5MTc0OH0.lnOqnq1AwN41g4xJ5O9oNIPBQqXYJkSrRhJ3osXtcsk';
 
       // 🔹 Créer le payload AVANT le fetch
       const payload = {
