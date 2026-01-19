0dd56660739565004afacd4c999a5106e7758b69
Kennouille <muhlemannk@gmail.com>
2026-01-19 17:30:27 -0600

    Amélioration

--- projet.js
+++ projet.js
@@ -1031,7 +1031,7 @@ async function showProjectDetails(projectId) {
         elements.archiveProjectBtn.style.display = project.archived ? 'none' : 'block';
         elements.archiveProjectBtn.textContent = project.archived ? 'Désarchiver' : 'Archiver';
 
-        showModal(elements.projectDetailsModal);
+        switchProjectTab('reservations');
 
     } catch (error) {
         console.error('Erreur affichage détails projet:', error);
